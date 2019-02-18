92c62a1d57431838589d47ac3c2c0d11439b961c
Mario Costa <you@example.com>
2019-02-18 17:46:11 +0000

    first commit

---
+++ src/E.h
@@ -0,0 +1,10 @@
+#ifndef _HEADER_E
+#define _HEADER_E
+
+class E {
+  public:
+  int e;
+};
+
+#endif
+
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+
+#include "core/A.h"
+#include "E.h"
+
+int main() {
+  A a;
+  E e;
+
+return 0;
+}
